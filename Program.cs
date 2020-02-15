4cdc5fd211c8e5a60668dfb800ff33a89bfbb93b
Chuck Francis <bnsf5622@gmail.com>
2020-02-15 10:32:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForAndTwentyBlackbirds.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birdsInPie = 0;
+                for (int i = 0; i < 2000000; i++)
+            {
+                Console.WriteLine("Blackbird #" + i + " goes into the pie");
+                birdsInPie++;
+
+
+            }
+
+            Console.WriteLine("There are " + birdsInPie + " birds in there!");
+            Console.WriteLine("Quite the pie is full");
+        }
+    }
+}
